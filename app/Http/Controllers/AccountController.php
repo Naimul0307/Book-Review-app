81d5945bb02b32f086518386225c64251ce311df
unknown <mdnaimul15902@gmail.com>
2024-05-09 15:14:14 +0400

    Make User Authentication

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
 
 class AccountController extends Controller
 {
@@ -39,5 +41,32 @@ class AccountController extends Controller
 
     public function login(){
         return view('account.login');
+    }
+
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' =>'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails())
+        {
+            return redirect()->route('account.login')->withInput()->withErrors($validator);
+        }
+
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            return redirect()->route('account.profile');
+        } else {
+            return redirect()->route('account.login')->with('error','Either email/password is incorrect!');
+        }
+    }
+
+    public function profile(){
+        return view('account.profile');
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
